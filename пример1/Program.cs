e2a510d1aa45eb3ca3278151c939e68c1615135c
Nastya_krevtka <nastyamkala1278@gmail.com>
2022-12-15 00:18:32 +0300

    задача1

---
+++ пример1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число: ");
+int chislo1 = int.Parse(Console.ReadLine());
+if (chislo1>99 && chislo1<1000)
+{
+ int num2 = chislo1/10;
+     num2 = num2%10;
+ Console.Write($"{chislo1} -> {num2}");  
+}
+else
+{
+Console.Write("введите трехзначное число");
+}
